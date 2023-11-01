e839ba7587a8c25ae55b6082813b6770d2fbc179
furkanlebit7 <furkanlebitt7@gmail.com>
2023-11-01 23:52:39 +0300

    Day 4 Updated

--- day4/MicroservicesInveonOrnek/Inveon.Services.Identity/Initializer/IDbInitializer.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Inveon.Services.Identity.Initializer
-{
-        public interface IDbInitializer
-    {
-        public void Initialize();
-    }
-}
--- day4/MicroservicesInveonOrnek/Inveon.Services.Identity/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Inveon.Services.Identity.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
